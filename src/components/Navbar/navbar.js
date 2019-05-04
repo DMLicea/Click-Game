bc984a73c8fa030c4046fc421a19a0d3dc439699
danielle <dmlicea@gmail.com>
2019-05-04 03:08:33 -0500

    navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -3,7 +3,14 @@ import "./navbar.css";
 
 const navbar = () => (
 
-<p></p>
+<nav className="navbar navbar-default navbar-fixed-top" id="catbar">
+
+         <ul>
+          <li className="left">Test Your Memory</li>
+          <li className="middle"></li>
+          <li className="right">Score:</li>
+        </ul>
+      </nav>
 
 );
 
