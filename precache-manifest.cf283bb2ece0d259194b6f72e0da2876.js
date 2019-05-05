55f854d34c6939a57cf93a545c6e8a767e6a7e90
danielle <dmlicea@gmail.com>
2019-05-04 23:55:25 -0500

    Updates

---
+++ precache-manifest.cf283bb2ece0d259194b6f72e0da2876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4677bf54511365990e97e7fe2af866dc",
+    "url": "/clickgame/index.html"
+  },
+  {
+    "revision": "dc4019338d756755b572",
+    "url": "/clickgame/static/css/main.9b833c18.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clickgame/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "dc4019338d756755b572",
+    "url": "/clickgame/static/js/main.10cbb83e.chunk.js"
+  },
+  {
+    "revision": "550c79de082ae6f7d9ff",
+    "url": "/clickgame/static/js/runtime~main.5bc58040.js"
+  }
+]);
